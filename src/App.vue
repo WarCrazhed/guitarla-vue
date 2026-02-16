c6c6b42e8f9cca3ea42982f48aafa404ef6780fe
Mario Zamora <zamora@Mac-mini-de-Mario-5.local>
2026-02-16 10:14:00 -0600

    Update quantities, delete and empty the shopping cart, display information about the main guitar, calculating the total to pay with (computed)

--- src/App.vue
+++ src/App.vue
@@ -7,24 +7,64 @@ import Footer from './components/Footer.vue';
 
 const guitarras = ref([])
 const carrito = ref([])
+const guitarra = ref({})
 
 onMounted(() => {
     guitarras.value = db;
+    guitarra.value = db[3];
 })
 
 const agregarCarrito = (guitarra) => {
-    guitarra.cantidad = 1;
-    carrito.value.push(guitarra)
+    const existeCarrito = carrito.value.findIndex(item => item.id === guitarra.id);
+    if (existeCarrito >= 0) {
+        carrito.value[existeCarrito].cantidad++;
+    } else {
+        guitarra.cantidad = 1;
+        carrito.value.push(guitarra)
+    }
 }
+
+const decrementarCantidad = (id) => {
+    const index = carrito.value.findIndex(item => item.id === id);
+    if (carrito.value[index].cantidad === 1) return;
+    carrito.value[index].cantidad--;
+}
+
+const incrementarCantidad = (id) => {
+    const index = carrito.value.findIndex(item => item.id === id);
+    if (carrito.value[index].cantidad >= 5) return;
+    carrito.value[index].cantidad++;
+}
+
+const eliminarProducto = (id) => {
+    carrito.value = carrito.value.filter(item => item.id !== id);
+}
+
+const vaciarCarrito = () => {
+    carrito.value = [];
+}
+
 </script>
 
 <template>
-    <Header :carrito="carrito" />
+    <Header 
+        :carrito="carrito" 
+        :guitarra="guitarra"
+        @decrementar-cantidad="decrementarCantidad"
+        @incrementar-cantidad="incrementarCantidad"
+        @agregar-carrito="agregarCarrito"
+        @eliminar-producto="eliminarProducto"
+        @vaciar-carrito="vaciarCarrito"
+    />
     <main class="container-xl mt-5">
         <h2 class="text-center">Nuestra Colección</h2>
 
         <div class="row mt-5">
-            <Guitarra v-for="guitarra in guitarras" :guitarra="guitarra" @agregar-carrito="agregarCarrito" />
+            <Guitarra 
+                v-for="guitarra in guitarras" 
+                :guitarra="guitarra" 
+                @agregar-carrito="agregarCarrito" 
+            />
         </div>
     </main>
     <Footer />
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,31 @@
 <script setup>
+import { computed } from 'vue';
+
 const props = defineProps({
     carrito: {
         type: Array,
         required: true,
+    }, 
+    guitarra: {
+        type: Object,
+        required: true,
     }
 })
+
+defineEmits([
+    'decrementar-cantidad', 
+    'incrementar-cantidad', 
+    'agregar-carrito',
+    'eliminar-producto',
+    'vaciar-carrito'
+])
+
+const totalPagar = computed(() => {
+    return props.carrito.reduce((total, producto) => {
+        return total + (producto.precio * producto.cantidad);
+    }, 0);
+})
+
 </script>
 
 <template>
@@ -44,16 +65,28 @@ const props = defineProps({
                                                 {{ producto.precio }}
                                             </td>
                                             <td class="flex align-items-start gap-4">
-                                                <button type="button" class="btn btn-dark">
+                                                <button 
+                                                    type="button" 
+                                                    class="btn btn-dark"
+                                                    @click="$emit('decrementar-cantidad', producto.id)"
+                                                >
                                                     -
                                                 </button>
                                                 {{ producto.cantidad }}
-                                                <button type="button" class="btn btn-dark">
+                                                <button 
+                                                    type="button" 
+                                                    class="btn btn-dark"
+                                                    @click="$emit('incrementar-cantidad', producto.id)"
+                                                >
                                                     +
                                                 </button>
                                             </td>
                                             <td>
-                                                <button class="btn btn-danger" type="button">
+                                                <button 
+                                                    class="btn btn-danger" 
+                                                    type="button"
+                                                    @click="$emit('eliminar-producto', producto.id)"
+                                                >
                                                     X
                                                 </button>
                                             </td>
@@ -61,8 +94,11 @@ const props = defineProps({
                                     </tbody>
                                 </table>
 
-                                <p class="text-end">Total pagar: <span class="fw-bold">$899</span></p>
-                                <button class="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
+                                <p class="text-end">Total pagar: <span class="fw-bold">${{ totalPagar }}</span></p>
+                                <button 
+                                    class="btn btn-dark w-100 mt-3 p-2"
+                                    @click="$emit('vaciar-carrito')"
+                                >Vaciar Carrito</button>
                             </div>
                         </div>
                     </div>
@@ -71,12 +107,14 @@ const props = defineProps({
 
             <div class="row mt-5">
                 <div class="col-md-6 text-center text-md-start pt-5">
-                    <h1 class="display-2 fw-bold">Modelo VAI</h1>
-                    <p class="mt-5 fs-5 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus,
-                        possimus quibusdam dolor nemo velit quo, fuga omnis, iure molestias optio tempore sint at ipsa
-                        dolorum odio exercitationem eos inventore odit.</p>
-                    <p class="text-primary fs-1 fw-black">$399</p>
-                    <button type="button" class="btn fs-4 bg-primary text-white py-2 px-5">Agregar al Carrito</button>
+                    <h1 class="display-2 fw-bold">Modelo {{ guitarra.nombre }}</h1>
+                    <p class="mt-5 fs-5 text-white">{{ guitarra.descripcion }}</p>
+                    <p class="text-primary fs-1 fw-black">${{ guitarra.precio }}</p>
+                    <button 
+                        type="button" 
+                        class="btn fs-4 bg-primary text-white py-2 px-5"
+                        @click="$emit('agregar-carrito', guitarra)"
+                    >Agregar al Carrito</button>
                 </div>
             </div>
         </div>
